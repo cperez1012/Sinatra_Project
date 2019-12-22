91acb3f5a4a3094ffa6ac1ea77caf27df1cb4916
Christian Perez <perezchristian1012@gmail.com>
2019-12-22 05:32:26 +0000

    still fixing investhub path details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,18 +25,27 @@ class UsersController < ApplicationController
     erb :'users/signup'
   end
 
-  post '/users' do
-    # binding.pry
-    if params[:username].empty? || params[:password].empty? || params[:email].empty? || params[:confirm_password].empty?
+  post '/signup' do
+  # binding.pry
+    if params[:username] == "" || params[:password] == "" || params[:email] == "" || params[:confirm_password] == ""
       redirect to '/signup'
-      # binding.pry
+    # binding.pry
     elsif params[:password] != params[:confirm_password]
       flash[:error] = "Your credentials were invalid. Try again!"
     else
-      user = User.create(username: params[:username], password: params[:password], email: params[:email])
-      session[:user_id] = @user.id
-      redirect to '/users/#{@user.id}'
-    end
+    user = User.create(username: params[:username], password: params[:password], email: params[:email])
+    session[:user_id] = user.id
+    redirect to '/investhub'
+  end
+end
+
+  post '/users' do
+    # will eventually need to add validations to confirm all inputs are filled out before creating user
+    @user = User.create(params)
+    # post sign up route to create user using params and add key/value pair to sessions hash
+    session[:user_id] = @user.id
+    # redirect to user profile
+    redirect "/users/#{@user.id}"
   end
 
   get '/logout' do
@@ -46,6 +55,15 @@ class UsersController < ApplicationController
       redirect to '/login'
     else
       redirect to '/'
+    end
+  end
+
+  get '/users/:slug' do
+    if logged_in? && current_user.slug == params[:slug]
+      @user = User.find_by_slug(params[:slug])
+      erb :'/users/show'
+    else
+      redirect to '/investhub'
     end
   end
 
