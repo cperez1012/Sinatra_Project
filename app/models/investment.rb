a9ae843fd57824d96fe42f6fbba63237b26566e3
Christian Perez <perezchristian1012@gmail.com>
2019-12-26 15:21:52 +0000

    changed file names in views folder

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,5 +1,12 @@
+require_relative "./concerns/slugifiable.rb"
+
 class Investment < ActiveRecord::Base
+
   belongs_to :user_id
   belongs_to :institution_id
 
+  validates :user_id, presence: true
+
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
 end
